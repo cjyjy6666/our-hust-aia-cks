f8feaf504d7dec55dba2eb65aac2b6a1a02f8040
Jiayu Chen <u202414389@hust.edu.cn>
2025-02-20 18:01:27 +0800

    做了light界面的退出和light按钮的参数传递

--- DISK_C/cks/include/light.h
+++ DISK_C/cks/include/light.h
@@ -1,6 +1,21 @@
 #ifndef __LIGHT_H__
 #define __LIGHT_H__
 
+// 在头文件中，使用防止重复声明的宏
+#ifndef CAR_LIGHT_H
+#define CAR_LIGHT_H
+
+struct car_light {
+    int right;
+    int left;
+    int fog;
+    int near_light;
+    int far_light;
+};
+
+#endif
+
+
 void light(int *puge);
 void draw_light_page();
 
--- DISK_C/cks/source/light.c
+++ DISK_C/cks/source/light.c
@@ -1,7 +1,11 @@
 #include"allfunc.h"
 
+void draw_light_page();
+void show_light(struct car_light light_status);
+
 void light(int *puge)
 {
+	struct car_light light_status={0,0,0,0,0};/*结构体在light.h中定义*/
 	mouse_off(&mouse);
 
 	draw_light_page();
@@ -13,15 +17,46 @@ void light(int *puge)
 
 	while (1)
 	{
+		show_light(light_status);
 		mouse_show(&mouse);
-		if (mouse_press(80, 70, 220, 200) == 1)
+		if (mouse_press(100,190,200,260) == 1)
 		{
-			*puge = 3;
+			light_status.right=light_status.right?0:1;
+		}
+		if (mouse_press(100,290,200,360) == 1)
+		{
+			light_status.left=light_status.left?0:1;
+		}
+		if (mouse_press(266,440,366,510) == 1)
+		{
+			light_status.fog=light_status.fog?0:1;
+		}
+		if (mouse_press(433,190,533,260) == 1)
+		{
+			light_status.near_light=light_status.near_light?0:1;
+		}
+		if (mouse_press(433,290,533,360) == 1)
+		{
+			light_status.far_light=light_status.far_light?0:1;	
+		}
+		if (mouse_press(266,63,366,150) == 1)
+		{
+			light_status.left=light_status.left?0:1;
+			light_status.right=light_status.right?0:1;
+		}
+		if (mouse_press(603,600,633,630) == 1)
+		{
+			*puge = 2;
 			break;
 		}
 	}
 
 }
+
+/*	留了两个小问题
+没有写双闪和左（右）转同时按下的情形
+双闪按钮的detect写的的矩形范围，可以改进成三角形区域
+*/
 
 void draw_light_page()
 {
@@ -31,7 +66,7 @@ void draw_light_page()
 	draw_botton(266,440,366,510,0x00FF,0x0000);
 	draw_botton(433,190,533,260,0x00FF,0x0000);
 	draw_botton(433,290,533,360,0x00FF,0x0000);
-	//鍙岄棯
+	//双闪
 	Line2(316,63,266,150,0xF800);
 	Line2(316,63,366,150,0xF800);
 	Line2(266,150,366,150,0xF800);
@@ -44,4 +79,13 @@ void draw_light_page()
     puthz(296, 465, "雾灯",24,30,0xFFFFFF);
     puthz(463, 215, "近光",24,30,0xFFFFFF); 
     puthz(463, 315, "远光",24,30,0xFFFFFF); 
+	Line2(603,600,633,600,0xFFFFFF);
+	Line2(603,600,603,630,0xFFFFFF);
+	Line2(603,600,633,630,0xFFFFFF);
+	Line2(633,600,603,630,0xFFFFFF);
+}
+
+void show_light(struct car_light light_status)
+{
+
 }
