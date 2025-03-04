833f85fb312960128a6b3719c13e6444f605094b
Jiayu Chen <u202414389@hust.edu.cn>
2025-03-04 23:17:21 +0800

    开始改mainpage

--- DISK_C/cks/source/mainpage.c
+++ DISK_C/cks/source/mainpage.c
@@ -32,32 +32,34 @@ void mainpage(int *puge)
 //然后从main函数开始做的
 void draw_mainpage()
 {
-	bar1(0, 0, 1024, 768,0xFFFFFF);
-	Line_Thick(633, 0, 633, 768, 1, 0x000000);
-	Line_Thick(0, 630, 633, 630, 1, 0x000000);
-	Circle(829, 194, 180, 0x000000);
-	Circle(829, 194, 179, 0x000000);
-	Circle(829, 194, 178, 0x000000);
-	Circle(829, 194, 177, 0x000000);
-	Circle(829, 194, 176, 0x000000);
-	bar1(640, 195, 1024, 768, 0xFFFFFF);
-	Line_Thick(649, 194, 1009, 194, 1, 0x000000);
-	Circle(829, 400, 180, 0x000000);
-	Circle(829, 400, 179, 0x000000);
-	Circle(829, 400, 178, 0x000000);
-	Circle(829, 400, 177, 0x000000);
-	Circle(829, 400, 176, 0x000000);
-	bar1(640, 400, 1024,768, 0xFFFFFF);
-	Line_Thick(649, 400, 1009, 400, 1, 0x000000);
+	bar1(0, 0, 1024, 768,0x0085);//和背景图中一样的深蓝色
+	//Readbmp64k(0,0,"bmp\\background.bmp");
+	Line_Thick(633, 0, 633, 768, 1, 0x7FFF);//和背景图中一样的亮浅绿色
+	Line_Thick(0, 630, 633, 630, 1, 0x7FFF);
+	Circle(829, 194, 180, 0x7FFF);
+	Circle(829, 194, 179, 0x7FFF);
+	Circle(829, 194, 178, 0x7FFF);
+	Circle(829, 194, 177, 0x7FFF);
+	Circle(829, 194, 176, 0x7FFF);
+	bar1(640, 195, 1024, 768, 0x0085);
+	Line_Thick(649, 194, 1009, 194, 1, 0x7FFF);
+	Circle(829, 400, 180, 0x7FFF);
+	Circle(829, 400, 179, 0x7FFF);
+	Circle(829, 400, 178, 0x7FFF);
+	Circle(829, 400, 177, 0x7FFF);
+	Circle(829, 400, 176, 0x7FFF);
+	bar1(640, 400, 1024,768, 0x0085);
+	Line_Thick(649, 400, 1009, 400, 1, 0x7FFF);
 	bar1(660, 450, 1000, 748, 0x000000);
-	draw_botton(80, 70, 220, 200,MAGENTA,MAGENTA);
+	Readbmp64k(0,0,"bmp\\222.bmp");
+	/*draw_botton(80, 70, 220, 200,MAGENTA,MAGENTA);
 	draw_botton(80, 250, 220, 380, 0x00FF, 0xDDDD);
 	draw_botton(80, 440, 220, 570, 0x00FF, 0xDDDD);
 	draw_botton(380, 70, 520, 200, 0x00FF, 0xDDDD);
 	draw_botton(380, 250, 520, 380, 0x00FF, 0xDDDD);
 	draw_botton(380, 440, 520, 570, 0x00FF, 0xDDDD);
 	puthz(110,119,"灯光",32,40,0xFFFFFF);
-	puthz(410,119,"空调",32,40,0xFFFFFF);
+	puthz(410,119,"空调",32,40,0xFFFFFF);*/
 }
 
 
